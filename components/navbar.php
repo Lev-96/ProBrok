71af1e78fc958f7f36141e15c18751902b9e4909
Levon Bakunts <levonbakunts3@gmail.com>
2024-11-30 00:48:54 +0400

    minor changes

--- components/navbar.php
+++ components/navbar.php
@@ -16,31 +16,31 @@
             <div class="col d-flex flex-column flex-sm-row align-items-center justify-content-center justify-content-sm-end">
                 <h4 class="nav_text text-align-center animate__animated animate__backInLeft"><?= $ml->tr('text_nav')?></h4>
                 <div class="d-flex align-items-center justify-content-center me-3 gap-3 phoneItem">
+                    <div class="dropdown mt-3 mt-sm-0">
+                        <button class="btn btn-light dropdown-toggle" type="button" id="languageDropdown"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="./images/flags/<?= array_values($selected)[0]['flag'] ?>"
+                                 alt="<?= array_keys($selected)[0] ?>"
+                                 style="height: 20px;">
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="languageDropdown">
+                            <?php foreach ($remainLang as $key => $value): ?>
+                                <li>
+                                    <a class="dropdown-item d-flex align-items-center" href="?lang=<?= $key ?>">
+                                        <img src="./images/flags/<?= $value['flag'] ?>"
+                                             alt="<?= $key ?>"
+                                             style="height: 20px;"
+                                             class="me-2">
+                                        <?= $value['name'] ?>
+                                    </a>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
                     <div class="d-flex align-items-center">
                         <img src="./images/icon/phoneIcon.png" alt="" class="phone-icon">
                         <a href="tel:+37494844452" class="text-decoration-none text-dark ms-2">+374 94 844 452</a>
                     </div>
-                </div>
-                <div class="dropdown mt-3 mt-sm-0">
-                    <button class="btn btn-light dropdown-toggle" type="button" id="languageDropdown"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="./images/flags/<?= array_values($selected)[0]['flag'] ?>"
-                             alt="<?= array_keys($selected)[0] ?>"
-                             style="height: 20px;">
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="languageDropdown">
-                        <?php foreach ($remainLang as $key => $value): ?>
-                            <li>
-                                <a class="dropdown-item d-flex align-items-center" href="?lang=<?= $key ?>">
-                                    <img src="./images/flags/<?= $value['flag'] ?>"
-                                         alt="<?= $key ?>"
-                                         style="height: 20px;"
-                                         class="me-2">
-                                    <?= $value['name'] ?>
-                                </a>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
                 </div>
             </div>
         </div>
